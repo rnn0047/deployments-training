3bb79fcb1fa177ff58fbe1519ed334bfec5ee14b
rnn0047 <ram.nagpure@gmail.com>
2019-08-23 22:42:01 -0400

    tt

--- api.py
+++ api.py
@@ -3,18 +3,31 @@ import tornado.web
 from allHandler import VersionHandler, AddHandler, DelHandler, GetHandler
 from book import Book
 
-books = Book()
+class BookApp:
+    def __init__(self):
+        self.books = Book()
+        self.port = 8000
 
-def create_app():
-    return tornado.web.Application([
-        (r"/v1", VersionHandler),
-        (r"/v1/addbook", AddHandler, dict(books = books)),
-        (r"/v1/delbook", DelHandler, dict(books = books)),
-        (r"/v1/getbooks", GetHandler, dict(books = books)),
-        ])
+    def run(self):
+        ba = self.create_app()
+        ba.listen(self.port)
+        print("created BookApp and listening on port {}".format(self.port))
+        tornado.ioloop.IOLoop.current().start()
+
+    def create_app(self):
+        return tornado.web.Application([
+            (r"/v1", VersionHandler),
+            (r"/v1/addbook", AddHandler, dict(books = self.books)),
+            (r"/v1/delbook", DelHandler, dict(books = self.books)),
+            (r"/v1/getbooks", GetHandler, dict(books = self.books)),
+            ])
+
+
+#for tornadoweb async web framework; use below format
+def main():
+    app = BookApp()
+    app.run()
 
 if __name__ == "__main__":
-    app = create_app()
-    print("created my webapp and listening on port 8890")
-    app.listen(8890)
-    tornado.ioloop.IOLoop.current().start()
+    main()
+
